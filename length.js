928892cef64e2b7ab613b4dae447758c5ae5e3ff
未知 <haruokame05@yahoo.co.jp>
2018-03-12 16:14:09 +0900

    first commit

---
+++ length.js
@@ -0,0 +1,11 @@
+﻿/*bookmarklet
+  ruler.js*/
+
+javascript:(function(){
+var ScrSize = "横幅" + window.parent.screen.width + "高さ" + window.parent.screen.height
+,WinSize = "横幅" + window.innerWidth + "高さ" + window.innerHeight
+,PageHeight = document.documentElement.scrollHeight || document.body.scrollHeight
+
+alert("画面サイズ：" + ScrSize,"ウィンドウサイズ：" + WinSize,
+"ページの長さ" + PageHeight);
+});
